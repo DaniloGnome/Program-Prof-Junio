7eb506bc5756671335ffb1af9771336f92846f93
DaniloGnome <127751001+DaniloGnome@users.noreply.github.com>
2023-03-19 22:58:23 -0300

    Update QuestaoSeteAlcoolEGasolina.java

--- QuestaoSeteAlcoolEGasolina.java
+++ QuestaoSeteAlcoolEGasolina.java
@@ -38,17 +38,18 @@ public class QuestaoSeteAlcoolEGasolina {
 		if (tipocombustivel .equals("G")) // aqui declaramos o .equals para comparacao ( tipo do combustivel é igual a G)
 		{
             precolitro = 3.79; // declaramos o preco da gasolina
-        } else if (tipocombustivel.equals("A")) {
+        } else if (tipocombustivel.equals("A"))  //// aqui declaramos o .equals para comparacao ( tipo do combustivel é igual a A)
+		{
             precolitro = 2.90; // declaramos o preço do alcool
         } else {
-            System.out.println("Tipo de combustível nao existente");
-            return;
+            System.out.println("Tipo de combustível nao existente"); // aqui o tipo de combustivel digitado nao foi A e nem G.
+            return; // o método "return;" é simplesmente encerrado, sem retornar nenhum valor.
         }
 
         double valortotal = precolitro * capacidadetanque;
-        System.out.printf("O valor total gasto para encher o tanque será de R$ %.2f\n", valortotal);
+        System.out.println("O valor total gasto para encher o tanque será de R$ " + valortotal); // aqui temos uma calcatenção 
 
-        sc.close();
+        sc.close(); // aqui fechamos o scanner
 	}
 
 }
