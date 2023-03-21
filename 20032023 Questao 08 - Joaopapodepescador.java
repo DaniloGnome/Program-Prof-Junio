458432030eff0e67702046776f3a574b12f317fc
DaniloGnome <127751001+DaniloGnome@users.noreply.github.com>
2023-03-20 21:04:12 -0300

    Update and rename Joaopapodepescador.java to 20032023 Questao 08 - Joaopapodepescador.java

--- 20032023 Questao 08 - Joaopapodepescador.java
+++ 20032023 Questao 08 - Joaopapodepescador.java
@@ -21,16 +21,16 @@ public class Joaopapodepescador {
 		Scanner sc = new Scanner(System.in);
 		DecimalFormat df = new DecimalFormat("##.00"); //aqui temos o decimalformat para introduzir na msg de excesso de peso
 		
-		System.out.println("Peso do peixe"); // print para pedir o peso do peixe
+		System.out.print("Digite o Peso do peixe :"); // print para pedir o peso do peixe
 		double pesopeixe = sc.nextDouble(); // variavel do tipo double para armazenar o valor do peixe na memoria
 		
 		if (pesopeixe >= 50) { // se peixe é maior que o valor 50
 			double passoupeso = pesopeixe - 50; // variavel do tipo double, diminuimos por 50 e a sobra será o valor armazenado no double 'passoupeso' 
 			double pesopermitido = passoupeso * 4;// variavel do tipo double, e o que sera armazenado sera o resultado da variavel 'passoupeso' vezes 4 que é o valor da multa por kg
-		System.out.println("O peixe passou "+ passoupeso + "kg. O total será : R$ " + df.format(pesopermitido)); // aqui temos, duas variaveis texto que sera impressa na tela e, usamos a formatacao df.format para a variavel 'pesopermitido'
+		System.out.print("O peixe passou "+ passoupeso + "kg. O total da multa será : R$ " + df.format(pesopermitido)); // aqui temos, duas variaveis texto que sera impressa na tela e, usamos a formatacao df.format para a variavel 'pesopermitido'
 		}
 		else {
-			System.out.println("O peixe está no peso permitido, nao houve excesso");
+			System.out.print("O peixe está no peso permitido, nao houve excesso");
 sc.close(); // fechamos o scanner
 	}
 }
