8637f7dbdf8e676c434e2562b0767262410412b7
DaniloGnome <127751001+DaniloGnome@users.noreply.github.com>
2023-03-21 11:21:28 -0300

    21032023 questao 8 encadeado

---
+++ questao08encadeado.java
@@ -0,0 +1,18 @@
+// Questão 08. Faça um algoritmo que receba um número e diga se este número está no
+// intervalo entre 100 e 200.
+public class questao08encadeado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 250;
+
+		if (a > 100) {
+			if (a < 200) {
+				System.out.println("verdadeiro");
+			} else {
+				System.out.println("falso");
+			}
+		}
+	}
+}
